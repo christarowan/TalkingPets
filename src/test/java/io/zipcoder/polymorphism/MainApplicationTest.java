db83fd3357fd7531a94c6f04f95f011edc678171
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-04-01 13:15:01 -0400

    added comments to test class

--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -1,8 +1,18 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainApplicationTest {
 @Test
-    public void testMe(){}
+    public void testMe() {
+
+
+
+
+// Anastasia is going to write tests for my program when she gets to it.
+// I already completed writing tests for her program and gave them to her.
+
+
+    }
 }
